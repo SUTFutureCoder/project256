c517dff397e3a7b5d7cb06b7aaa7e5285dcbea00
linxingchen <linxingchen@iwaimai.baidu.com>
2018-03-08 00:43:14 +0800

    update sql

--- project256.sql
+++ project256.sql
@@ -1,13 +1,13 @@
 # 文章表
 CREATE TABLE `essay` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `essay_id` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '文章id',
+  `essay_id` char(32) NOT NULL DEFAULT '' COMMENT '文章id',
   `essay_title` varchar(32) NOT NULL DEFAULT '' COMMENT '文章标题',
   `essay_content` text NOT NULL COMMENT '文章内容',
-  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态',
-  `create_user` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建人',
-  `create_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建时间',
-  `update_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态',
+  `create_user` char(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `create_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
   PRIMARY KEY (`id`),
   KEY `essay_id` (`essay_id`),
   KEY `create_user` (`create_user`),
@@ -17,12 +17,12 @@ CREATE TABLE `essay` (
 # 心愿表
 CREATE TABLE `wish` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `wish_id` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '心愿id',
+  `wish_id` char(32) NOT NULL DEFAULT '' COMMENT '心愿id',
   `wish_content` text NOT NULL COMMENT '心愿内容',
-  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态',
-  `create_user` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建人',
-  `create_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建时间',
-  `update_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态',
+  `create_user` char(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `create_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
   PRIMARY KEY (`id`),
   KEY `wish_id` (`wish_id`),
   KEY `create_user` (`create_user`),
@@ -32,13 +32,13 @@ CREATE TABLE `wish` (
 # 子心愿表
 CREATE TABLE `little_wish` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `little_wish_id` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '子心愿id',
+  `little_wish_id` char(32) NOT NULL DEFAULT '' COMMENT '子心愿id',
   `little_wish_content` text NOT NULL COMMENT '心愿内容',
-  `wish_id`  bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '心愿id',
-  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态',
-  `create_user` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建人',
-  `create_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建时间',
-  `update_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `wish_id` char(32) NOT NULL DEFAULT '' COMMENT '心愿id',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态',
+  `create_user` char(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `create_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
   PRIMARY KEY (`id`),
   KEY `little_wish_id` (`little_wish_id`),
   KEY `wish_id` (`wish_id`),
@@ -49,13 +49,13 @@ CREATE TABLE `little_wish` (
 # 心愿文章表
 CREATE TABLE `re_wish_essay` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `wish_id` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '心愿id',
-  `little_wish_id` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '子心愿id',
-  `essay_id` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '文章id',
-  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态',
-  `create_user` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建人',
-  `create_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '创建时间',
-  `update_time` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `wish_id` char(32) NOT NULL DEFAULT '' COMMENT '心愿id',
+  `little_wish_id` char(32) NOT NULL DEFAULT '' COMMENT '子心愿id',
+  `essay_id` char(32) NOT NULL DEFAULT '' COMMENT '文章id',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态',
+  `create_user` char(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `create_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
   PRIMARY KEY (`id`),
   KEY `wish_id` (`wish_id`),
   KEY `little_wish_id` (`little_wish_id`),
@@ -63,4 +63,3 @@ CREATE TABLE `re_wish_essay` (
   KEY `create_user` (`create_user`),
   KEY `create_time` (`create_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='心愿文章关联表';
-
